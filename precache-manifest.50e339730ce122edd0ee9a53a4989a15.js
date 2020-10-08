cf4f7467539559a65585c618cf972c11089db61c
dangus21 <angusmiguel21@gmail.com>
2020-10-08 18:27:01 +0000

    Deploying to gh-pages from  @ b4fb8f29f97b265ac17efa14d27a1ce5cc351cb6 🚀

---
+++ precache-manifest.50e339730ce122edd0ee9a53a4989a15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c7bdb1d6053b4d60c4487eddd34ff6",
+    "url": "/Bingo/index.html"
+  },
+  {
+    "revision": "a1dafb5bae3b83fe857e",
+    "url": "/Bingo/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "372a86095ffa0b23e188",
+    "url": "/Bingo/static/css/main.69574aa9.chunk.css"
+  },
+  {
+    "revision": "a1dafb5bae3b83fe857e",
+    "url": "/Bingo/static/js/2.98805346.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/Bingo/static/js/2.98805346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "372a86095ffa0b23e188",
+    "url": "/Bingo/static/js/main.91c305b5.chunk.js"
+  },
+  {
+    "revision": "f458d6954bb4882981b1",
+    "url": "/Bingo/static/js/runtime-main.00b8c666.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bf6dad86b50859180292637819922082.js"
+  "/Bingo/precache-manifest.50e339730ce122edd0ee9a53a4989a15.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Bingo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
